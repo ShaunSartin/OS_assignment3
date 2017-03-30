a5fc8a7ccf5597a4a5ffa6660e85fdb3f9738ba5
ShaunSartin <luckydice2323@yahoo.com>
2017-03-30 01:23:25 -0500

    ls now displays the entries in the root directory.
    seg-faults are an issue if a file system isnt opened yet.

--- mfs.c
+++ mfs.c
@@ -34,6 +34,7 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <stdint.h>
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
@@ -91,18 +92,44 @@ int main()
 
     // BEGIN STUDENT CODE IN MAIN()
 
-    // Rename/Relocate data, so that the variable name is more intuitive
+    // Rename data, so that the variable name is more intuitive
     char *cmd = token[0];
     char *arg1 = token[1];
 
     //NOTE: ADD DESCRIPTION
-    short bps;
+    int16_t bps;
 
     //NOTE: ADD DESCRIPTION
-    short spc;
+    int8_t spc;
 
     //NOTE: ADD DESCRIPTION
-    short rsc;
+    int16_t rsc;
+
+    //NOTE: ADD DESCRIPTION
+    int8_t numFATS;
+
+    //NOTE: ADD DESCRIPTION
+    char volLab[12];
+
+    //NOTE: ADD DESCRIPTION
+    int32_t FATSz32;
+
+    //NOTE: ADD DESCRIPTION
+    int rootStart;
+
+    //NOTE: ADD DESCRIPTION
+    struct  __attribute__ ((__packed__)) DirectoryEntry {
+    	char DIR_Name[12];
+    	uint8_t DIR_Attr;
+    	uint8_t Unused1[8];
+    	uint16_t DIR_FirstClusterHigh;
+    	uint8_t Unused2[4];
+    	uint16_t DIR_FirstClusterLow;
+    	uint32_t DIR_FileSize;
+    };
+
+    //NOTE: ADD DESCRIPTION
+    struct DirectoryEntry dir[16]; 
 
     // This variable is used to determine which file system is open. 
     // That way, this program can handle the case where the user tries to close a file system that has not been opened.
@@ -131,6 +158,29 @@ int main()
 	{
 		printf("File image: %s was successfully opened.\n", arg1);
 		strcpy(open_file_sys, arg1);
+
+		//Skip to BPB_BytsPerSec
+		fseek(fp, 11, SEEK_SET);
+
+		//Read BPB_BytsPerSec
+		fread(&bps, 1, 2, fp);
+
+		//Read BPB_SecPerClus
+		fread(&spc, 1, 1, fp);
+
+		//Read BPB_RsvdSecCnt
+		fread(&rsc, 1, 2, fp);
+
+		//Read BPB_NumFATS
+		fread(&numFATS, 1, 1, fp);
+
+		//Skip to BPB_FATSz32
+		fseek(fp, 19, SEEK_CUR);
+
+		//Read BPB_FATSz32
+		fread(&FATSz32, 1, 4, fp);
+
+		rootStart = (numFATS * FATSz32 * bps) + (rsc * bps);		
 	}
     }
 
@@ -161,22 +211,69 @@ int main()
         // Therefore, we can perform the 'info' operation.
 	if(strcmp(open_file_sys, "") != 0)
 	{
-		//Skip to BPB_BytsPerSec
-		fseek(fp, 11, SEEK_SET);
-
-		//Read BPB_BytsPerSec
-		fread(&bps, 2, 1, fp);
-		printf("BytesPerSec: %hi\n", bps);
-
-		//Read BPB_SecPerClus
-		fread(&spc, 1, 1, fp);
-		printf("SecPerClus: %hi\n", spc);
-		
-		//Read BPB_RsvdSecCnt
-		fread(&rsc, 2, 1, fp);
-		printf("RsvdSecCnt: %hi\n", rsc);
-	}    
+		printf("Bytes Per Sector \t in Decimal: %5d\t In Hex: %4x\n", bps, bps);
+		printf("Sectors Per Cluster \t in Decimal: %5d\t In Hex: %4x\n", spc, spc);
+		printf("Reserved Sector Count \t in Decimal: %5d\t In Hex: %4x\n", rsc, rsc);
+		printf("Number of FATS \t\t in Decimal: %5d\t In Hex: %4x\n", numFATS, numFATS);
+		printf("FATSz32 \t\t in Decimal: %5d\t In Hex: %4x\n", FATSz32, FATSz32);
+	}   
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	} 
     }
+    else if(strcmp(cmd, "volume") == 0)
+    {
+
+	if(strcmp(open_file_sys, "") != 0)
+	{	
+		//Skip to BS_VolLab
+		fseek(fp, 71, SEEK_SET);
+
+		//Read BS_VolLab
+		//We must null-terminate the string so that extra garbage doesn't print out. 
+		fread(volLab, 1, 11, fp);
+		volLab[11] = '\0';
+		printf("VolLab: %s\n", volLab);
+	}
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
+    }
+
+    else if(strcmp (cmd, "ls") == 0)
+    {
+	if(strcmp(open_file_sys, "") != 0)
+	{
+		// Move to the start of the root directory.
+		// That way, we can read all of its data.
+		fseek(fp, rootStart, SEEK_SET);
+		for(int i = 0; i < 17; i++)
+		{
+			fread(dir[i].DIR_Name, 1, 11, fp);
+			fread(&dir[i].DIR_Attr, 1, (uint8_t) 1, fp);
+			dir[i].DIR_Name[11] = '\0';
+					
+			// If the filename's first character has a hex-value of 0xE5 or 0x05, the file has been deleted.
+			// Therefore, we do not show it.
+			// If the file's attribute has a hex-value of 0x01, 0x10, or 0x20.
+			if (((dir[i].DIR_Name[0] != (char) 5) && (dir[i].DIR_Name[0] != (char) 229)) && 
+				((dir[i].DIR_Attr == (uint8_t) 1) || (dir[i].DIR_Attr == (uint8_t) 16) || (dir[i].DIR_Attr == (uint8_t) 32)))
+			{
+				printf("%s\n", dir[i].DIR_Name);
+			}
+			fseek(fp, 20, SEEK_CUR);
+		}	
+	}	
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
+		
+    }
+
+    printf("\n");
     // END STUDENT CODE IN MAIN()
     free( working_root );
 
