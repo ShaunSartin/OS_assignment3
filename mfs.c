1b624bd69da32fad392ee5db289234cd14a3de2a
ShaunSartin <luckydice2323@yahoo.com>
2017-03-31 23:58:00 -0500

    fcmp works properly. Seg-faults still occur.
    Seg-faults mainly occur when command arguments are left null.

--- mfs.c
+++ mfs.c
@@ -35,7 +35,7 @@
 #include <string.h>
 #include <signal.h>
 #include <stdint.h>
-
+#include <ctype.h>
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
                                 // In this case  white space
@@ -43,11 +43,78 @@
 
 #define MAX_COMMAND_SIZE 255    // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 10     // Mav shell only supports ten arguments
+#define MAX_NUM_ARGUMENTS 10     // mfs.c only supports ten arguments
+
+
+// Function: Filename Comparison
+// Purpose: Modify user-inputted filename to see it matches the corresponding filename stored in the directory entry
+// Returns: 0 if match, -1 if no match
+int fcmp(char *uname, char *dname)
+{
+	int i;
+
+	// This is used as an intermediary string. This will hold the converted version of uname.
+	char cname[11];
+
+	// Used to calculate the length of the user-inputted filename. This is used in the comparison.
+	int ulen = 1;
+
+	// If there is a dot in the user-inputted filename, the dotIndex will be the position of that character.
+	// Otherwise, dotIndex will be equal to -1.
+	int dotIndex = -1;
+
+	// Convert every value in the user-inputted filename to an uppercase character as that is what is in the directory entries.
+	for(i = 0; uname[i]; i++)
+	{
+		uname[i] = (char) toupper(uname[i]);
+		if(uname[i] == '.')
+		{
+			dotIndex = i;
+		}
+		ulen += 1;
+	}
+
+	if(dotIndex != -1)
+	{
+
+		// Copy every value from uname to cname up to the '.' character.
+		for(i = 0; i < dotIndex; i++)
+		{
+			cname[i] = uname[i];	
+		}
+		
+		// If applicable, append spaces so that the format is similar to the directory entry's name.
+		for(; i < 8; i++)
+		{
+			cname[i] = ' ';
+		}
+
+		// Add the file extension in the last three indexes.
+		for(; i < 11; i++)
+		{
+			cname[i] = uname[(dotIndex + 1) + (i - 8)];
+		}
+
+	}
+	printf("Cname: %s \nDname: %s\n", cname, dname);
+	
+	if(strcmp(cname, dname) == 0)
+	{
+		return 0;
+	}
+
+	return -1;
+}
 
 int main()
 {
 
+  // This variable is used to determine which file system is open. 
+  // That way, this program can handle the case where the user tries to close a file system that has not been opened.
+  // This is out of the loop so that memset doesn't run every iteration.
+  char open_file_sys[MAX_COMMAND_SIZE];
+  memset(open_file_sys, 0, MAX_COMMAND_SIZE);
+  
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
 
   while( 1 )
@@ -95,6 +162,8 @@ int main()
     // Rename data, so that the variable name is more intuitive
     char *cmd = token[0];
     char *arg1 = token[1];
+    char *arg2 = token[2];
+    char *arg3 = token[3];
 
     //NOTE: ADD DESCRIPTION
     int16_t bps;
@@ -116,6 +185,9 @@ int main()
 
     //NOTE: ADD DESCRIPTION
     int rootStart;
+    
+    //NOTE: ADD DESCRIPTION
+    int i;
 
     //NOTE: ADD DESCRIPTION
     struct  __attribute__ ((__packed__)) DirectoryEntry {
@@ -130,10 +202,6 @@ int main()
 
     //NOTE: ADD DESCRIPTION
     struct DirectoryEntry dir[16]; 
-
-    // This variable is used to determine which file system is open. 
-    // That way, this program can handle the case where the user tries to close a file system that has not been opened.
-    char open_file_sys[MAX_COMMAND_SIZE];
 
     // This file pointer will be used to determine whether or not the fat32.img file is open.
     FILE *fp;
@@ -181,6 +249,26 @@ int main()
 		fread(&FATSz32, 1, 4, fp);
 
 		rootStart = (numFATS * FATSz32 * bps) + (rsc * bps);		
+		
+		// Move to the start of the root directory.
+		// That way, we can read all of its data.
+		// NOTE: Remove print.
+		fseek(fp, rootStart, SEEK_SET);
+		for(i = 0; i < 16; i++)
+		{
+			// Read all the needed data from the directory and store it in its appropriate index.
+			fread(dir[i].DIR_Name, 1, 11, fp);
+			fread(&dir[i].DIR_Attr, 1, (uint8_t) 1, fp);
+			fseek(fp, 8, SEEK_CUR);
+			fread(&dir[i].DIR_FirstClusterHigh, 1, 2, fp);
+			fseek(fp, 4, SEEK_CUR);
+			fread(&dir[i].DIR_FirstClusterLow, 1, 2, fp);
+			fread(&dir[i].DIR_FileSize, 1, 4, fp);
+			
+			// Add Null-Terminator to the end of the filename.
+			dir[i].DIR_Name[11] = '\0';
+		}	
+		fseek(fp, rootStart, SEEK_SET);
 	}
     }
 
@@ -234,7 +322,14 @@ int main()
 		//We must null-terminate the string so that extra garbage doesn't print out. 
 		fread(volLab, 1, 11, fp);
 		volLab[11] = '\0';
-		printf("VolLab: %s\n", volLab);
+		if(volLab[0] != ' ')
+		{
+			printf("VolLab: %s\n", volLab);
+		}
+		else
+		{
+			printf("Error: volume name not found.\n");
+		}
 	}
 	else
 	{
@@ -246,24 +341,19 @@ int main()
     {
 	if(strcmp(open_file_sys, "") != 0)
 	{
-		// Move to the start of the root directory.
-		// That way, we can read all of its data.
-		fseek(fp, rootStart, SEEK_SET);
-		for(int i = 0; i < 17; i++)
+		printf("Name       |Attr   |HClus |LClus     |Size \n");
+		for(i = 0; i < 16; i++)
 		{
-			fread(dir[i].DIR_Name, 1, 11, fp);
-			fread(&dir[i].DIR_Attr, 1, (uint8_t) 1, fp);
-			dir[i].DIR_Name[11] = '\0';
-					
 			// If the filename's first character has a hex-value of 0xE5 or 0x05, the file has been deleted.
 			// Therefore, we do not show it.
 			// If the file's attribute has a hex-value of 0x01, 0x10, or 0x20.
 			if (((dir[i].DIR_Name[0] != (char) 5) && (dir[i].DIR_Name[0] != (char) 229)) && 
 				((dir[i].DIR_Attr == (uint8_t) 1) || (dir[i].DIR_Attr == (uint8_t) 16) || (dir[i].DIR_Attr == (uint8_t) 32)))
 			{
-				printf("%s\n", dir[i].DIR_Name);
+				//NOTE: Only need to print the filename.
+				printf("%s \t %d \t %d \t %d \t %d \n", dir[i].DIR_Name, dir[i].DIR_Attr, 
+					dir[i].DIR_FirstClusterHigh, dir[i].DIR_FirstClusterLow, dir[i].DIR_FileSize);
 			}
-			fseek(fp, 20, SEEK_CUR);
 		}	
 	}	
 	else
@@ -271,6 +361,64 @@ int main()
 		printf("Error: File system image must be opened first.\n");
 	}
 		
+    }
+
+    else if(strcmp (cmd, "read") == 0)
+    {
+	if(strcmp(open_file_sys, "") != 0)
+	{
+		for(i = 0; i < 16; i++)
+		{
+			if(fcmp(arg1, dir[i].DIR_Name) == 0)
+			{
+				printf("Filename found!\n");
+				break;
+			}	
+			else
+			{
+				printf("Filename not found at index %d\n", i);
+			}
+		}	
+	}
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
+
+    }
+
+    else if(strcmp (cmd, "stat") == 0)
+    {
+	if(strcmp(open_file_sys, "") != 0)
+	{	
+		for(i = 0; i < 16; i++)
+		{
+			if(strcmp(arg1, dir[i].DIR_Name) == 0)
+			{
+				printf("Filename found!\n");
+				break;
+			}	
+			else
+			{
+				printf("Filename not found at index %d\n", i);
+			}
+		}	
+	}
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
+    }
+
+    else if(strcmp(cmd, "cd") == 0)
+    {
+	if(strcmp(open_file_sys, "") != 0)
+	{
+	}	
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
     }
 
     printf("\n");
