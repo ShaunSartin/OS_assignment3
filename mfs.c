5dacbf454c06f195ea3fdc5fcc3499c4fa9ab482
ShaunSartin <luckydice2323@yahoo.com>
2017-04-08 22:02:46 -0500

    CD now works.
    CD now works properly (assuming correct input. See seg-fault bug). Volume Label needs fixing.

--- mfs.c
+++ mfs.c
@@ -47,7 +47,8 @@
 
 
 // Function: Filename Comparison
-// Purpose: Modify user-inputted filename to see it matches the corresponding filename stored in the directory entry
+// Purpose: Modify user-inputted filename to see it matches the corresponding filename stored in the directory entry.
+// 	    Subdirectories are considered to be files, and thus will work with this function.
 // Returns: 0 if match, -1 if no match
 int fcmp(char *uname, char *dname)
 {
@@ -73,16 +74,30 @@ int fcmp(char *uname, char *dname)
 		}
 		ulen += 1;
 	}
+	
 
-	if(dotIndex != -1)
+	if((strcmp(uname, ".") == 0) || (strcmp(uname, "..") == 0))
+	{
+		for(i = 0; i < 11; i++)
+		{
+			cname[i] = dname[i];
+		}
+	
+	        if(strcmp(cname, dname) == 0)
+		{
+			return 0;
+		}
+	}
+
+	else if(dotIndex != -1)
 	{
 
 		// Copy every value from uname to cname up to the '.' character.
 		for(i = 0; i < dotIndex; i++)
 		{
-			cname[i] = uname[i];	
+			cname[i] = uname[i];
 		}
-		
+
 		// If applicable, append spaces so that the format is similar to the directory entry's name.
 		for(; i < 8; i++)
 		{
@@ -96,25 +111,93 @@ int fcmp(char *uname, char *dname)
 		}
 
 	}
-	printf("Cname: %s \nDname: %s\n", cname, dname);
 	
+	//Check to see if 'uname' is a directory
+	else
+	{
+		// Copy every value from uname to cname until there are no more characters in uname.
+		// Note that we do not wish to copy the null-terminator.
+		for(i = 0; i < (ulen - 1); i++)
+		{
+			cname[i] = uname[i];
+		}
+
+		// If applicable, append spaces to cname so that the format is similar to the directory entry's name.
+		for (; i < 11; i++)
+		{
+			cname[i] = ' ';
+		}
+		cname[11] = '\0';
+	}
+
+	// DEBUG printf("Cname: %s \tDname: %s \tUlen: %d \tClen: %d \tDlen: %d \n", cname, dname, ulen, strlen(cname), strlen(dname));
 	if(strcmp(cname, dname) == 0)
 	{
+		//NOTE: Delete print
+		printf("FILE FOUND: %s\n", cname);
 		return 0;
 	}
 
 	return -1;
 }
 
+// Function: Recursive Read
+// Purpose: Read the data from a file in chunks. Grants ability to read bytes from multiple clusters.
+// Returns: void
+// NOTE: How to ensure that we stay at same pointer.
+
+// arg2 and arg3 acts as a 'bytes-left' counter
+// arg2 will be 0 once the beginning position has been found.
+// arg3 will be 0 once the last character has been read.
+void rread(FILE* fp, int rootStart, long arg2, long arg3, int currClusNum, int numOfClusters)
+{
+	int i;
+	char buffer[512];
+
+	// Recursively go through and determine which cluster contains the initial offset.
+	// I.E. 1st cluster, 2nd cluster, etc. (This is held by numOfClusters, which should always intially be 1)
+	if(arg2 > 512)
+	{
+		rread(fp, rootStart, arg2 - 512, arg3, currClusNum, numOfClusters + 1);
+	}
+
+	// Then go to that particular cluster
+	else
+	{
+		// Reference the FAT to determine what the cluster number is.
+		// This determines the actual cluster number.
+		// note: the FAT starts at the 163840th byte
+		for(int i = 0; i < numOfClusters; i++)
+		{
+			currClusNum = 163840 + (currClusNum * 4);
+		}
+
+		// Fseek to the data for that cluster
+		fseek(fp, rootStart + (currClusNum * 512) + arg2, SEEK_SET);
+		if (arg3 <= 512)
+		{
+			fread(buffer, 1, arg3, fp);
+			printf("%s\n", buffer);
+			return;
+		}
+	}
+}
+
 int main()
 {
 
-  // This variable is used to determine which file system is open. 
+  // This variable is used to determine which file system is open.
   // That way, this program can handle the case where the user tries to close a file system that has not been opened.
   // This is out of the loop so that memset doesn't run every iteration.
   char open_file_sys[MAX_COMMAND_SIZE];
   memset(open_file_sys, 0, MAX_COMMAND_SIZE);
-  
+
+  // Keeps track of all the directories and their location in the clusters
+  int dir_location[5];
+
+  // Keeps track of what index in dir_location we are currently at. (i.e. current working directory)
+  int cwd = 0;
+
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
 
   while( 1 )
@@ -185,9 +268,12 @@ int main()
 
     //NOTE: ADD DESCRIPTION
     int rootStart;
-    
+
     //NOTE: ADD DESCRIPTION
     int i;
+
+    //NOTE: ADD DESCRIPTION
+    int file_flag = 0;
 
     //NOTE: ADD DESCRIPTION
     struct  __attribute__ ((__packed__)) DirectoryEntry {
@@ -201,7 +287,7 @@ int main()
     };
 
     //NOTE: ADD DESCRIPTION
-    struct DirectoryEntry dir[16]; 
+    struct DirectoryEntry dir[16];
 
     // This file pointer will be used to determine whether or not the fat32.img file is open.
     FILE *fp;
@@ -211,8 +297,8 @@ int main()
 	free(working_root);
 	return 0;
     }
-    
-    // Attempt to open the file system image and, if it is opened, store the name in the 'open_file_sys' string. 
+
+    // Attempt to open the file system image and, if it is opened, store the name in the 'open_file_sys' string.
     else if(strcmp(cmd, "open") == 0)
     {
 
@@ -248,11 +334,10 @@ int main()
 		//Read BPB_FATSz32
 		fread(&FATSz32, 1, 4, fp);
 
-		rootStart = (numFATS * FATSz32 * bps) + (rsc * bps);		
-		
+		rootStart = (numFATS * FATSz32 * bps) + (rsc * bps);
+
 		// Move to the start of the root directory.
 		// That way, we can read all of its data.
-		// NOTE: Remove print.
 		fseek(fp, rootStart, SEEK_SET);
 		for(i = 0; i < 16; i++)
 		{
@@ -264,10 +349,11 @@ int main()
 			fseek(fp, 4, SEEK_CUR);
 			fread(&dir[i].DIR_FirstClusterLow, 1, 2, fp);
 			fread(&dir[i].DIR_FileSize, 1, 4, fp);
-			
+
 			// Add Null-Terminator to the end of the filename.
 			dir[i].DIR_Name[11] = '\0';
-		}	
+		}
+		dir_location[0] = rootStart;
 		fseek(fp, rootStart, SEEK_SET);
 	}
     }
@@ -304,22 +390,22 @@ int main()
 		printf("Reserved Sector Count \t in Decimal: %5d\t In Hex: %4x\n", rsc, rsc);
 		printf("Number of FATS \t\t in Decimal: %5d\t In Hex: %4x\n", numFATS, numFATS);
 		printf("FATSz32 \t\t in Decimal: %5d\t In Hex: %4x\n", FATSz32, FATSz32);
-	}   
+	}
 	else
 	{
 		printf("Error: File system image must be opened first.\n");
-	} 
+	}
     }
     else if(strcmp(cmd, "volume") == 0)
     {
 
 	if(strcmp(open_file_sys, "") != 0)
-	{	
+	{
 		//Skip to BS_VolLab
 		fseek(fp, 71, SEEK_SET);
 
 		//Read BS_VolLab
-		//We must null-terminate the string so that extra garbage doesn't print out. 
+		//We must null-terminate the string so that extra garbage doesn't print out.
 		fread(volLab, 1, 11, fp);
 		volLab[11] = '\0';
 		if(volLab[0] != ' ')
@@ -347,20 +433,48 @@ int main()
 			// If the filename's first character has a hex-value of 0xE5 or 0x05, the file has been deleted.
 			// Therefore, we do not show it.
 			// If the file's attribute has a hex-value of 0x01, 0x10, or 0x20.
-			if (((dir[i].DIR_Name[0] != (char) 5) && (dir[i].DIR_Name[0] != (char) 229)) && 
+			if (((dir[i].DIR_Name[0] != (char) 5) && (dir[i].DIR_Name[0] != (char) 229)) &&
 				((dir[i].DIR_Attr == (uint8_t) 1) || (dir[i].DIR_Attr == (uint8_t) 16) || (dir[i].DIR_Attr == (uint8_t) 32)))
 			{
 				//NOTE: Only need to print the filename.
-				printf("%s \t %d \t %d \t %d \t %d \n", dir[i].DIR_Name, dir[i].DIR_Attr, 
+				printf("%s \t %d \t %d \t %d \t %d \n", dir[i].DIR_Name, dir[i].DIR_Attr,
 					dir[i].DIR_FirstClusterHigh, dir[i].DIR_FirstClusterLow, dir[i].DIR_FileSize);
 			}
-		}	
-	}	
+		}
+	}
 	else
 	{
 		printf("Error: File system image must be opened first.\n");
 	}
-		
+
+    }
+
+    else if(strcmp(cmd, "stat") == 0)
+    {
+	if (strcmp(open_file_sys, "") != 0)
+	{
+		for (i = 0; i < 16; i++)
+		{
+			if (fcmp(arg1, dir[i].DIR_Name) == 0)
+			{
+				file_flag = 1;
+				break;
+			}
+		}
+		if (file_flag == 1)
+		{
+			printf("Attributes: %d\nLow Cluster Number: %d\n", dir[i].DIR_Attr, dir[i].DIR_FirstClusterLow);
+			file_flag = 0;
+		}
+		else
+		{
+			printf("Error: File not found.\n");
+		}
+	}
+	else
+	{
+		printf("Error: File system image must be opened first.\n");
+	}
     }
 
     else if(strcmp (cmd, "read") == 0)
@@ -371,50 +485,108 @@ int main()
 		{
 			if(fcmp(arg1, dir[i].DIR_Name) == 0)
 			{
-				printf("Filename found!\n");
+				file_flag = 1;
 				break;
-			}	
-			else
-			{
-				printf("Filename not found at index %d\n", i);
 			}
-		}	
-	}
-	else
-	{
-		printf("Error: File system image must be opened first.\n");
-	}
-
-    }
-
-    else if(strcmp (cmd, "stat") == 0)
-    {
-	if(strcmp(open_file_sys, "") != 0)
-	{	
-		for(i = 0; i < 16; i++)
+		}
+		if(file_flag == 1)
 		{
-			if(strcmp(arg1, dir[i].DIR_Name) == 0)
-			{
-				printf("Filename found!\n");
-				break;
-			}	
-			else
-			{
-				printf("Filename not found at index %d\n", i);
-			}
-		}	
+			//NOTE: FIX THIS. What happens if we reach the end of the cluster? multiple clusters?
+			//Check user-input to ensure they enter the correct data-type.
+			fseek(fp, (long int) arg2, SEEK_SET);
+			//rread();
+			file_flag = 0;
+		}
+		else
+		{
+			printf("Error: File not found.\n");
+		}
 	}
 	else
 	{
 		printf("Error: File system image must be opened first.\n");
 	}
+
     }
 
     else if(strcmp(cmd, "cd") == 0)
     {
+
 	if(strcmp(open_file_sys, "") != 0)
 	{
-	}	
+		for (i = 0; i < 16; i++)
+		{
+			// Look for subdirectories
+			// Note that subdirectories have the value of 16 (decimal) for their attribute.
+			// DEBUG: printf("arg1: %s, DIR_Name: %s, DIR_Attr: %d\n", arg1, dir[i].DIR_Name, dir[i].DIR_Attr);
+			if ((fcmp(arg1, dir[i].DIR_Name) == 0) && (dir[i].DIR_Attr == 16))
+			{
+				file_flag = 1;
+				break;
+			}
+		}
+		if (file_flag == 1)
+		{
+			if (strcmp(arg1, ".") == 0)
+			{
+			}
+		
+			else if (strcmp(arg1, "..") == 0)
+			{
+				// If we're already at the root directory, we do nothing.
+				if (cwd != 0)
+				{
+					fseek(fp, dir_location[cwd - 1], SEEK_SET);
+					for(i = 0; i < 16; i++)
+                                	{
+                                        	// Read all the needed data from the directory and store it in its appropriate index.
+                                        	fread(dir[i].DIR_Name, 1, 11, fp);
+                                        	fread(&dir[i].DIR_Attr, 1, (uint8_t) 1, fp);
+                                        	fseek(fp, 8, SEEK_CUR);
+                                        	fread(&dir[i].DIR_FirstClusterHigh, 1, 2, fp);
+                	                        fseek(fp, 4, SEEK_CUR);
+               	        	                fread(&dir[i].DIR_FirstClusterLow, 1, 2, fp);
+        	                                fread(&dir[i].DIR_FileSize, 1, 4, fp);
+	
+                                        	// Add Null-Terminator to the end of the filename.
+                                	        dir[i].DIR_Name[11] = '\0';
+                                	}
+                                	fseek(fp, dir_location[cwd - 1], SEEK_SET);
+                                	//DEBUG printf("FP is now at: %d\n", dir_location[cwd - 1]); 
+					cwd -= 1;
+				}
+				
+			}
+
+			else
+			{	
+				cwd += 1;
+				dir_location[cwd] = rootStart + ((dir[i].DIR_FirstClusterLow * 512) - 1024);	
+				fseek(fp, rootStart + ((dir[i].DIR_FirstClusterLow * 512) - 1024), SEEK_SET);
+				for(i = 0; i < 16; i++)
+				{
+					// Read all the needed data from the directory and store it in its appropriate index.
+					fread(dir[i].DIR_Name, 1, 11, fp);
+					fread(&dir[i].DIR_Attr, 1, (uint8_t) 1, fp);
+					fseek(fp, 8, SEEK_CUR);
+					fread(&dir[i].DIR_FirstClusterHigh, 1, 2, fp);
+					fseek(fp, 4, SEEK_CUR);
+					fread(&dir[i].DIR_FirstClusterLow, 1, 2, fp);
+					fread(&dir[i].DIR_FileSize, 1, 4, fp);
+
+					// Add Null-Terminator to the end of the filename.
+					dir[i].DIR_Name[11] = '\0';
+				}
+				fseek(fp, dir_location[cwd], SEEK_SET);
+				//DEBUG printf("FP is now at: %d\n", dir_location[cwd]);
+			}
+			file_flag = 0;
+		}
+		else
+		{
+			printf("Error: Directory not found.\n");
+		}
+	}
 	else
 	{
 		printf("Error: File system image must be opened first.\n");
